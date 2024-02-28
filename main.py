f54bdd991bbae5c735322721baed63a0b32e53e5
Hituh <okazysssie@gmail.com>
2024-02-28 12:54:07 +0100

    Init

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+
+def main():
+    print("This is the main function.")
+
+
+if __name__ == "__main__":
+    # Code inside this block will only be executed when main.py is run directly
+    main()
